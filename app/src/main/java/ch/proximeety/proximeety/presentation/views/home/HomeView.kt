7728cf34a8d434e27abbe749617d59ff02f9c1f8
Hamzezi <hamza.laarous@epfl.ch>
2022-03-17 10:13:46 +0100

    implement a basic structure of the home view

--- app/src/main/java/ch/proximeety/proximeety/presentation/views/home/HomeView.kt
+++ app/src/main/java/ch/proximeety/proximeety/presentation/views/home/HomeView.kt
@@ -1,13 +1,19 @@
 package ch.proximeety.proximeety.presentation.views.home
 
 import androidx.activity.compose.BackHandler
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Divider
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import ch.proximeety.proximeety.util.extensions.getActivity
 
@@ -25,10 +31,103 @@ fun HomeView(
         context.getActivity()?.finish()
     }
 
+    Scaffold(
+        topBar = {
+            homeTopBar()
+        },
+        bottomBar = {
+            homeBottomBar()
+        }) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceEvenly,
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(),
+        ) {
+            Text(
+                text = "Displaying ${viewModel.state.value.user?.displayName}'s feed",
+                modifier = Modifier.padding(all = 5.dp),
+                style = MaterialTheme.typography.h3,
+                textAlign = TextAlign.Center
+            )
+
+        }
+        Divider(color = Color.Black)
+    }
+}
+
+/**
+ * The HomeView's top bar
+ */
+@Composable
+fun homeTopBar() {
     Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
+        modifier = Modifier
+            .fillMaxWidth()
+//        .background(color = Color.LightGray),
     ) {
-        Text("Welcome ${viewModel.state.value.user?.displayName}")
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween, modifier = Modifier
+                .fillMaxWidth()
+                .height(50.dp), verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "map",
+                style = MaterialTheme.typography.body1,
+                modifier = Modifier.padding(all = 5.dp)
+            )
+
+            Text(
+                text = "Proximeety", modifier = Modifier.padding(all = 5.dp),
+                style = MaterialTheme.typography.h6,
+                textAlign = TextAlign.Center
+            )
+
+            Text(
+                text = "msg",
+                style = MaterialTheme.typography.body1,
+                modifier = Modifier.padding(all = 5.dp)
+            )
+        }
+    }
+}
+
+/**
+ * The HomeView's bottom bar
+ */
+@Preview
+@Composable
+fun homeBottomBar() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+//        .border(color = Color.Black, width = 1.dp)
+//        .background(color = Color.LightGray)
+    ) {
+        Divider(color = Color.Black)
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween, modifier = Modifier
+                .fillMaxWidth()
+                .height(50.dp), verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "Search",
+                style = MaterialTheme.typography.body1,
+                modifier = Modifier.padding(all = 5.dp)
+            )
+
+            Text(
+                text = "Camera",
+                style = MaterialTheme.typography.body1,
+                modifier = Modifier.padding(all = 5.dp)
+            )
+
+            Text(
+                text = "Profile",
+                style = MaterialTheme.typography.body1,
+                modifier = Modifier.padding(all = 5.dp)
+            )
+        }
     }
 }
