0ccb5a235c8f290c742722dc4f40231e2deb6493
De Busschere Yanis <yanisdebusschere@gmail.com>
2022-03-17 17:59:58 +0100

    Add bluetooth nearby friends

--- app/src/main/java/ch/proximeety/proximeety/presentation/views/authenticationHome/AuthenticationHomeEvent.kt
+++ app/src/main/java/ch/proximeety/proximeety/presentation/views/authenticationHome/AuthenticationHomeEvent.kt
@@ -1,10 +1,8 @@
 package ch.proximeety.proximeety.presentation.views.authenticationHome
 
-import ch.proximeety.proximeety.util.SyncActivity
-
 /**
  * An event from the View to the ViewModel for the Authentication Home View.
  */
 sealed class AuthenticationHomeEvent {
-    class AuthenticateWithGoogle(val activity: SyncActivity) : AuthenticationHomeEvent()
+    object AuthenticateWithGoogle : AuthenticationHomeEvent()
 }
