de7f6b3db2c7bd94ac522c7ad348f0aa06d12f50
azizdhouib00 <aziz.dhouib@epfl.ch>
2022-03-17 17:24:55 +0100

    First commit for the settings UI

---
+++ app/src/main/java/ch/proximeety/proximeety/presentation/views/settings/SettingsViewModel.kt
@@ -0,0 +1,12 @@
+package ch.proximeety.proximeety.presentation.views.settings
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+/**
+ * The ViewModel for the Settings View.
+ */
+@HiltViewModel
+class SettingsViewModel @Inject constructor(): ViewModel() {
+}
