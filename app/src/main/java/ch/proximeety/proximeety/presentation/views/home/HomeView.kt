a873f305f328d4834eb6baf885da66f45b4d0f98
Hamzezi <hamza.laarous@epfl.ch>
2022-03-17 12:51:32 +0100

    update HomeView to use components

--- app/src/main/java/ch/proximeety/proximeety/presentation/views/home/HomeView.kt
+++ app/src/main/java/ch/proximeety/proximeety/presentation/views/home/HomeView.kt
@@ -12,9 +12,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import ch.proximeety.proximeety.presentation.views.home.components.homeBottomBar
+import ch.proximeety.proximeety.presentation.views.home.components.homeTopBar
 import ch.proximeety.proximeety.util.extensions.getActivity
 
 /**
@@ -57,77 +58,5 @@ fun HomeView(
     }
 }
 
-/**
- * The HomeView's top bar
- */
-@Composable
-fun homeTopBar() {
-    Box(
-        modifier = Modifier
-            .fillMaxWidth()
-//        .background(color = Color.LightGray),
-    ) {
-        Row(
-            horizontalArrangement = Arrangement.SpaceBetween, modifier = Modifier
-                .fillMaxWidth()
-                .height(50.dp), verticalAlignment = Alignment.CenterVertically
-        ) {
-            Text(
-                text = "map",
-                style = MaterialTheme.typography.body1,
-                modifier = Modifier.padding(all = 5.dp)
-            )
 
-            Text(
-                text = "Proximeety", modifier = Modifier.padding(all = 5.dp),
-                style = MaterialTheme.typography.h6,
-                textAlign = TextAlign.Center
-            )
 
-            Text(
-                text = "msg",
-                style = MaterialTheme.typography.body1,
-                modifier = Modifier.padding(all = 5.dp)
-            )
-        }
-    }
-}
-
-/**
- * The HomeView's bottom bar
- */
-@Preview
-@Composable
-fun homeBottomBar() {
-    Box(
-        modifier = Modifier
-            .fillMaxWidth()
-//        .border(color = Color.Black, width = 1.dp)
-//        .background(color = Color.LightGray)
-    ) {
-        Divider(color = Color.Black)
-        Row(
-            horizontalArrangement = Arrangement.SpaceBetween, modifier = Modifier
-                .fillMaxWidth()
-                .height(50.dp), verticalAlignment = Alignment.CenterVertically
-        ) {
-            Text(
-                text = "Search",
-                style = MaterialTheme.typography.body1,
-                modifier = Modifier.padding(all = 5.dp)
-            )
-
-            Text(
-                text = "Camera",
-                style = MaterialTheme.typography.body1,
-                modifier = Modifier.padding(all = 5.dp)
-            )
-
-            Text(
-                text = "Profile",
-                style = MaterialTheme.typography.body1,
-                modifier = Modifier.padding(all = 5.dp)
-            )
-        }
-    }
-}
