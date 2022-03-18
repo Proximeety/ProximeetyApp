0095866f7df1468bebae424860fb398cc28bab49
belengg27 <belen01gomez@gmail.com>
2022-03-18 09:54:26 +0100

    Fixed error with User initialization

--- app/src/main/java/ch/proximeety/proximeety/presentation/views/friends/FriendsModel.kt
+++ app/src/main/java/ch/proximeety/proximeety/presentation/views/friends/FriendsModel.kt
@@ -6,32 +6,33 @@ import ch.proximeety.proximeety.core.entities.User
  * The Model for the Friends View.
  */
 var user1: User = User(
+    "1",
     "User 1",
     "user1",
     "user1",
-    "user1@gmail.com",
-    "1"
+    "user1@gmail.com"
 )
 var user2: User = User(
+    "2",
     "User 2",
     "user2",
     "user2",
     "user2@gmail.com",
-    "2"
+
 )
 var user3: User = User(
+    "3",
     "User 3",
     "user3",
     "user3",
-    "user3@gmail.com",
-    "1"
+    "user3@gmail.com"
 )
 var user4: User = User(
+    "4",
     "User 4",
     "user4",
     "user4",
-    "user4@gmail.com",
-    "1"
+    "user4@gmail.com"
 )
 
 
