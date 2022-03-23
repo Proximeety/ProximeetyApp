2aeea1bb6590ed5d7181c115e1e7890ba297994f
azizdhouib00 <aziz.dhouib@epfl.ch>
2022-03-23 04:30:15 +0100

    Added Profile UI, not done yet, missing unit tests

---
+++ app/src/main/java/ch/proximeety/proximeety/presentation/views/userProfile/UserProfileModel.kt
@@ -0,0 +1,10 @@
+package ch.proximeety.proximeety.presentation.views.userProfile
+
+import ch.proximeety.proximeety.core.entities.User
+
+/**
+ * The Model for the User Profile View.
+ */
+data class UserProfileModel (
+    var user: User?
+)
