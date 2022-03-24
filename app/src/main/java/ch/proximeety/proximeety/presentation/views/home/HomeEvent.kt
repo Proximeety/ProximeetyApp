74053cee042fc0465573514711e3f4486c2e5895
Hamzezi <hamza.laarous@epfl.ch>
2022-03-24 19:26:57 +0100

    Add HomeView

--- app/src/main/java/ch/proximeety/proximeety/presentation/views/home/HomeEvent.kt
+++ app/src/main/java/ch/proximeety/proximeety/presentation/views/home/HomeEvent.kt
@@ -6,4 +6,10 @@ package ch.proximeety.proximeety.presentation.views.home
 sealed class HomeEvent {
     object NavigateToNearbyUsersViewModel : HomeEvent()
     object SignOut : HomeEvent()
+    object NavigateToMapView : HomeEvent()
+    object NavigateToMessagingView: HomeEvent()
+    object NavigateToProfileView: HomeEvent()
+    object NavigateToSearchBarView: HomeEvent()
+    object NavigateToNearbyUsersView: HomeEvent()
+    object NavigateToCameraView: HomeEvent()
 }
