827ef89041540ab87b955e57a606006f3d63d450
belengg27 <belen01gomez@gmail.com>
2022-03-31 18:55:54 +0200

    Added test

--- app/src/androidTest/java/ch/proximeety/proximeety/presentation/views/friends/FriendsViewTest.kt
+++ app/src/androidTest/java/ch/proximeety/proximeety/presentation/views/friends/FriendsViewTest.kt
@@ -2,59 +2,73 @@ package ch.proximeety.proximeety.presentation.views.friends
 
 import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import ch.proximeety.proximeety.core.interactions.UserInteractions
 import ch.proximeety.proximeety.di.AppModule
 import ch.proximeety.proximeety.presentation.MainActivity
+import ch.proximeety.proximeety.presentation.navigation.NavigationManager
 import ch.proximeety.proximeety.presentation.theme.ProximeetyTheme
+import ch.proximeety.proximeety.presentation.views.home.HomeView
+import ch.proximeety.proximeety.presentation.views.home.HomeViewModel
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import dagger.hilt.android.testing.UninstallModules
+import kotlinx.coroutines.runBlocking
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import javax.inject.Inject
 
 @HiltAndroidTest
 @UninstallModules(AppModule::class)
 class FriendsViewTest {
-    @get:Rule(order = 0)
+
+    @get:Rule
     val hiltRule = HiltAndroidRule(this)
 
-    @get:Rule(order = 1)
+    @get:Rule
     val composeTestRule = createAndroidComposeRule<MainActivity>()
+
+    @Inject
+    lateinit var navigationManager: NavigationManager
+
+    @Inject
+    lateinit var userInteractions: UserInteractions
+
+    private lateinit var viewModel: FriendsViewModel
 
     @Before
     fun setup() {
+        hiltRule.inject()
+
+        runBlocking {
+            userInteractions.authenticateWithGoogle()
+        }
+
+        viewModel = FriendsViewModel(navigationManager, userInteractions)
+
         composeTestRule.setContent {
             ProximeetyTheme {
-                FriendsView()
+                FriendsView(viewModel)
             }
         }
     }
 
     @Test
     fun friendListTest() {
-        composeTestRule.onNodeWithText("Tom").assertExists()
-        composeTestRule.onNodeWithText("Tomas").assertExists()
-        composeTestRule.onNodeWithText("Mery").assertExists()
-        composeTestRule.onNodeWithText("Mark").assertExists()
-        composeTestRule.onNodeWithText("user1@gmail.com").assertExists()
-        composeTestRule.onNodeWithText("user2@gmail.com").assertExists()
-        composeTestRule.onNodeWithText("user3@gmail.com").assertExists()
-        composeTestRule.onNodeWithText("user4@gmail.com").assertExists()
-        composeTestRule.onAllNodesWithContentDescription("User profile picture")
+        composeTestRule.onNodeWithText("Test User").assertExists()
+        composeTestRule.onNodeWithText("null").assertExists()
     }
 
     @Test
     fun searchBarTest() {
-        composeTestRule.onNodeWithTag("textField").performClick().performTextInput("tom")
+        composeTestRule.onNodeWithTag("textField").performClick().performTextInput("test")
         composeTestRule.onNodeWithTag("button").performClick()
-        composeTestRule.onNodeWithText("Tom").assertExists()
-        composeTestRule.onNodeWithText("Tomas").assertExists()
+        composeTestRule.waitForIdle()
+        composeTestRule.onNodeWithText("Test User").assertExists()
+    }
 
-        composeTestRule.onNodeWithTag("textField").performClick().performTextClearance()
-        composeTestRule.onNodeWithTag("button").performClick()
-        composeTestRule.onNodeWithText("Tom").assertExists()
-        composeTestRule.onNodeWithText("Tomas").assertExists()
-        composeTestRule.onNodeWithText("Mery").assertExists()
-        composeTestRule.onNodeWithText("Mark").assertExists()
+    @Test
+    fun linkToProfile() {
+        composeTestRule.onNodeWithText("Test User").performClick()
     }
 }
