f2d6aa4d55bcac1b73f27d92233eaab23357ff89
azizdhouib00 <91628116+azizdhouib00@users.noreply.github.com>
2022-04-08 07:57:13 +0200

    Delete app/src/main/java/ch/proximeety/proximeety/presentation/views/profile directory

--- app/src/main/java/ch/proximeety/proximeety/presentation/views/profile/ProfileEvent.kt
+++
@@ -1,9 +0,0 @@
-package ch.proximeety.proximeety.presentation.views.profile
-
-/**
- * An event from the View to the ViewModel for the Nearby Users View.
- */
-sealed class ProfileEvent {
-    object AddAsFriend : ProfileEvent()
-    object SignOut : ProfileEvent()
-}
