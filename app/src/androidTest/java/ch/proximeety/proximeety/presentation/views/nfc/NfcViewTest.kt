eaca572b2c366a0a5f30d9033ad7d18162cb8ab0
Francis Yang <francisyangsr@outlook.com>
2022-05-19 18:24:03 +0200

    fi tests

--- app/src/androidTest/java/ch/proximeety/proximeety/presentation/views/nfc/NfcViewTest.kt
+++ app/src/androidTest/java/ch/proximeety/proximeety/presentation/views/nfc/NfcViewTest.kt
@@ -70,7 +70,7 @@ class NfcViewTest {
 
     @Test
     fun createNfcTag() {
-        composeTestRule.waitUntil(1000) { viewModel.nfcTag.value != null}
+        composeTestRule.waitUntil { viewModel.isNewTag.value != null}
         composeTestRule.onNodeWithText("Yes").performClick()
     }
 
